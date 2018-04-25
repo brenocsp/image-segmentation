36e50a372daa53839aa39e05da64fe52aebbf819
brenocsp <brenocsp@gmail.com>
2018-04-25 11:00:06 -0300

    V2

--- TP1.c
+++ TP1.c
@@ -35,7 +35,7 @@ Autor: Breno Claudio de Sena Pimenta
 typedef struct tipo_pixel {
     int cor;
     bool verificador;
-}tipo_pixel;
+}tipo_pixel;//teste
 
 typedef struct tipo_imagem_pgm {
     int altura, largura, nivel;
